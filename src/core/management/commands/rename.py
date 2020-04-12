9383cecbdd7d789cf239ca4cda86c61213e88eb0
Jeremy Gehlen <46670139+JSGehlen@users.noreply.github.com>
2020-04-12 15:50:53 +0300

    Update rename.py

--- src/core/management/commands/rename.py
+++ src/core/management/commands/rename.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
         new_project_name = kwargs['new_project_name']
 
         # bit of logic to rename the project
-        files_to_rename = ['boilerplate/settings/base.py', 'boilerplate/wsgi.py', 'manage.py']
-        folder_to_rename = 'boilerplate'
+        files_to_rename = ['myproject/settings/base.py', 'myproject/wsgi.py', 'manage.py']
+        folder_to_rename = 'myproject'
 
         for f in files_to_rename:
             with open(f, 'r') as file:
                 filedata = file.read()
-            filedata = filedata.replace('boilerplate', new_project_name)
+            filedata = filedata.replace('myproject', new_project_name)
 
             with open(f, 'w') as file:
                 file.write(filedata)
